ae13f6d4114f3347c9095b78e83937f28ac6fa2a
jamisonlima <jamison.lima@gmail.com>
2024-10-16 10:09:12 -0300

    lint judge.py

--- judge.py
+++ judge.py
@@ -1,11 +1,11 @@
 class Solution:
-    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+    def find_judge(self, n: int, trust: List[List[int]]) -> int:
         trusted: List[int] = [0]*n
         trusts: List[int] = [0]*n
         for l in trust:
             trusts[l[0]-1] += 1
             trusted[l[1]-1] += 1
-            
+
         for i in range(n):
             if trusted[i] == n-1 and not trusts[i]:
                 return i+1
