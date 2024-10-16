7458511bd3ca564559767138ebbd295144160663
jamisonlima <jamison.lima@gmail.com>
2024-10-16 18:42:48 -0300

    fix: typo

--- codes/star.py
+++ codes/star.py
@@ -2,5 +2,5 @@ from collections import Counter
 
 class Solution:
     def find_center(self, edges: List[List[int]]) -> int:
-        "on star graph all edgees connect to center"
+        "on star graph all edges connect to center"
         return edges[0][0] if edges[0][0] in (edges[1][1], edges[1][0]) else edges[0][1]
