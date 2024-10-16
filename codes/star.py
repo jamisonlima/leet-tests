d2321ef2f7f9d817b8d941287aa980fa6db6cedf
jamisonlima <jamison.lima@gmail.com>
2024-10-16 18:39:47 -0300

    Create star.py

---
+++ codes/star.py
@@ -0,0 +1,6 @@
+from collections import Counter
+
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        "on star graph all edgees connect to center"
+        return edges[0][0] if edges[0][0] in (edges[1][1], edges[1][0]) else edges[0][1]
